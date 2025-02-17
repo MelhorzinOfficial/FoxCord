05d115b4c270c63a66c72bfcf74f25313ccdad28
Gabriel Raposo <gabriel.henrique7@hotmail.com>
2025-02-17 17:10:19 -0300

    feat(commands): adicionar novos comandos de voz
    - Importar comandos de voz: limite, renomear e transferir.
    - Adicionar comando de ping para verificação de status.
    
    Essas alterações expandem a funcionalidade do bot, permitindo interações mais ricas com os usuários através de novos comandos de voz, melhorando a experiência geral do usuário e a versatilidade do bot.

---
+++ src/commands/index.ts
@@ -0,0 +1,11 @@
+import * as ping from "./info/ping";
+import * as limite from "./voice/limit";
+import * as renomear from "./voice/rename";
+import * as transferir from "./voice/transfer";
+
+export const commands = {
+  ping,
+  limite,
+  renomear,
+  transferir,
+};
