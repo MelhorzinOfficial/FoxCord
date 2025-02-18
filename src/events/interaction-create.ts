6d47396bd14f7958a945ecc20ee48b8d1fa8f4fc
MatheusVp2 <teteuoliveira12@gmail.com>
2025-02-18 12:07:29 -0300

    feat(interaction): enhance command handling and add voice channel cleanup logic

--- src/events/interaction-create.ts
+++ src/events/interaction-create.ts
@@ -1,9 +1,42 @@
+import { Command } from '@/commands/impl';
+import { getObjectImports } from '@/utils/object-imports';
 import { CacheType, Events, Interaction } from 'discord.js';
+import * as allCommands from '../commands';
 import { EventImpl } from './impl';
 
 class EventInteractionCreate implements EventImpl<Events.InteractionCreate> {
+	
 	type: string = Events.InteractionCreate;
-	listener(interaction: Interaction<CacheType>) {}
+
+	commands: Map<string, Command> = new Map();
+
+	constructor() {
+		this.listener = this.listener.bind(this);
+		const cmds = getObjectImports<Command>(allCommands);
+		this.commands = new Map(cmds.map((cmd) => [cmd.data.name, cmd]));
+	}
+
+	listener(interaction: Interaction<CacheType>) {
+		if (!interaction.isCommand()) return;
+		if (!interaction.isChatInputCommand()) return;
+
+		const command = this.commands.get(interaction.commandName);
+		if (!command) {
+			return interaction.reply({ content: 'Command not found', ephemeral: true });
+		};
+
+		try {
+			console.log(`Executing command: ${command.data.name}`);
+			command.execute(interaction);
+		} catch (error) {
+			console.error("Error while executing command ", error);
+			if (interaction.deferred || interaction.replied) {
+				return interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+			}
+			return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+
+	}
 }
 
 export const eventInteractionCreate = new EventInteractionCreate();
--- src/events/voice-state-update.ts
+++ src/events/voice-state-update.ts
@@ -1,11 +1,21 @@
-import { Events, VoiceState } from 'discord.js';
+import { CategoryChannel, Events, VoiceChannel, VoiceState } from 'discord.js';
+import { env } from '../config/env';
 import { EventImpl } from './impl';
 
 class EventVoiceStateUpdate implements EventImpl<Events.VoiceStateUpdate> {
 	type: string = Events.VoiceStateUpdate;
 
 	listener(oldState: VoiceState, newState: VoiceState) {
-		console.log('Voice state update');
+		
+		const parentChannel = newState.guild.channels.cache.get(env.VOICE_CHANNEL_GENERATOR_ID);
+		const category = parentChannel instanceof CategoryChannel ? parentChannel : null;
+
+		if (category) {
+			category.children.cache.filter((channel): channel is VoiceChannel => channel instanceof VoiceChannel && channel.id !== env.VOICE_CHANNEL_GENERATOR_ID && channel.members.size === 0 && channel.name.startsWith("🚀"))
+			.forEach((channel) => {channel.delete().catch(console.error);});
+		}
+
+
 	}
 }
 
